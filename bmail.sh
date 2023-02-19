6f9e3d56ac1a7c50e22d7bd452df5ad4e580aa54
lgo <lgo@black-cats.org>
2023-02-19 16:38:42 +0100

    add script to use gmail within brave browser

---
+++ bmail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Start Gmail app in a google-chrome window
+
+URL="https://mail.google.com/mail/u/1/#inbox"
+USER_DATA_DIR="${HOME}/.config/google-chrome-for-tools/gmail"
+
+[ -d ${USER_DATA_DIR} ] || mkdir -p ${USER_DATA_DIR}
+
+brave-browser --user-data-dir=${USER_DATA_DIR} ${URL} &> /dev/null &
+#google-chrome --user-data-dir=${USER_DATA_DIR} --app=${URL} &> /dev/null &
